b5ef70717c28e9b9f4ef4a84e75db263720eb7e9
Junior Francisco <francisco.alves@libertyseguros.com.br>
2021-08-16 23:08:43 -0300

    Exercicios - Criação de filtros

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -14,10 +15,46 @@ export class EventosComponent implements OnInit {
   }
   set filtroLista(value: string) {
     this._filtroLista = value;
-    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this.filtroLista, "Tema") : this.eventos;
   }
 
-  eventosFiltrados: any [];
+  _filtroLocal: string;
+  get filtroLocal(): string {
+    return this._filtroLocal;
+  }
+  set filtroLocal(value: string){
+    this._filtroLocal = value;
+    this.eventosFiltrados = this._filtroLocal ? this.filtrarEventos(this.filtroLocal, "Local") : this.eventos;
+  }
+
+  _filtroData: string;
+  get filtroData(): string {
+    return this._filtroData;
+  }
+  set filtroData(value: string){
+    this._filtroData = value;
+    this.eventosFiltrados = this._filtroData ? this.filtrarEventos(this.filtroData, "Data") : this.eventos;
+  }
+
+  _filtroQtdPessoas: string;
+  get filtroQtdPessoas(): string {
+    return this._filtroQtdPessoas;
+  }
+  set filtroQtdPessoas(value: string){
+    this._filtroQtdPessoas = value;
+    this.eventosFiltrados = this._filtroQtdPessoas ? this.filtrarEventos(this.filtroQtdPessoas, "QtdPessoas") : this.eventos;
+  }
+
+  _filtroLote: string;
+  get filtroLote(): string {
+    return this._filtroLote;
+  }
+  set filtroLote(value: string){
+    this._filtroLote = value;
+    this.eventosFiltrados = this._filtroLote ? this.filtrarEventos(this.filtroLote, "Lote") : this.eventos;
+  }
+
+  eventosFiltrados: any [];  
   eventos: any = [];
   imagemLargura = 50;
   imagemMargem  = 2;
@@ -29,11 +66,37 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
-  filtrarEventos(filtrarPor: string): any {
+  filtrarEventos(filtrarPor: string, Tipo: string): any {
     filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-    );
+    if (Tipo == "Tema")  {   
+      return this.eventos.filter(
+        evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      );
+    }
+
+    if (Tipo == "Local")  {   
+      return this.eventos.filter(
+        evento => evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      );
+    }
+
+    if (Tipo == "Data")  {   
+      return this.eventos.filter(
+        evento => evento.dataEvento.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      );
+    }
+
+    if (Tipo == "QtdPessoas")  {   
+      return this.eventos.filter(
+        evento => evento.qtdPessoas.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      );
+    }
+
+    if (Tipo == "Lote")  {   
+      return this.eventos.filter(
+        evento => evento.lote.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      );
+    }
   }
   
   alternarImagem() {
